03b022bd2a5a7a69112c9d633ee8b88b2a9085b9
leventefazakas-web <levente.fazakas@snapsoft.io>
2026-06-04 16:15:50 +0200

    Update main.tf
    corrected subnets

--- main.tf
+++ main.tf
@@ -29,14 +29,14 @@ resource "aws_internet_gateway" "gw" {
 resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
+  availability_zone       = "eu-central-1a"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "public_2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-1b"
+  availability_zone       = "eu-centralb"
   map_public_ip_on_launch = true
 }
 
