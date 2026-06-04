9069679d64bced302c0aa07e4f9d5afdcc00aba1
leventefazakas-web <levente.fazakas@snapsoft.io>
2026-06-04 16:17:51 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_subnet" "public_1" {
 resource "aws_subnet" "public_2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "eu-centralb"
+  availability_zone       = "eu-central-1b"
   map_public_ip_on_launch = true
 }
 
